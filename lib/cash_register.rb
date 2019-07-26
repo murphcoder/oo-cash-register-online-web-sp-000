fa288bd3d6bd811d820c8b037dc6acb39b820564
Sean Murphy <filmmakersean@fastmail.net>
2019-07-26 00:08:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
-
+class CashRegister
+  
+  attr_reader :total
+  @discount = 0
+  @grocery_list = {}
+  
+  def initialize(discount = 1)
+    @discount = discount
+    @total = 0
+  end
+  
+  def monetize(number)
+    '%.2f' % ((number.to_f) / 100)
+  end
+  
+  def demonetize(number)
+    ((number.to_f) * 100).to_i
+  end
+  
+  def add_item(item,price,quantity = 1)
+    
+  end
+  
+end
